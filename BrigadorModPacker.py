0ac98ab0af2db9b38280e5f45120f3550c6caf28
maximombro <maximombro@gmail.com>
2018-08-01 21:16:02 -0400

    Packer can now add MECH data to the appropriate spot in global

--- BrigadorModPacker.py
+++ BrigadorModPacker.py
@@ -5,8 +5,18 @@
 import os
 import json
 
+# Config
+# Category Name for any vehicle mods to be placed under
+CATEGORY_NAME = "VARIOUS | SNC Requisistions"
+
+# Globals
+GLOBAL_JSON = {}
+
 # Main Thread
 def main():
+    # Globals
+    global GLOBAL_JSON
+
     # Print welcome
     print("[ BRIGADOR MOD PACKER ]")
 
@@ -30,34 +40,76 @@ def main():
 
     # Report
     print(str(len(modDirs))+" mod files found")
+    print(modDirs)
     print("\nLocating global.json...")
 
     # Find global.json
     gblDir = "../../data/global.json"
     if os.path.isfile(gblDir):
         print("global.json found")
-        gblFile = open(gblDir,"r")
+        gblFile = open(gblDir,"r",encoding="latin-1")
+        GLOBAL_JSON = json.load(gblFile, encoding="latin-1")
     else:
         print("Could not find global.json. Exiting.")
         exit()
 
     # Report
-    print("Parsing mod files...")
+    print("Parsing and adding mod files...")
 
     # Loop through all mod files
     for path in modDirs:
         # Open and load json
-        print(path)
         with open(path, "r") as jsonFile:
             try:
                 # Parse json data
                 data = json.load(jsonFile)
-                print(data['archetype'])
             except Exception:
                 print(path+" is not a valid .json file")
+            else:
+                # Add mod to global
+                addModToGlobal(data, path)
 
-    # Close global.json
+    # Close Global
     gblFile.close()
+
+# Adds the given json mod to global
+def addModToGlobal(data, path):
+    if data['archetype'] == "MECH":
+        # Look for mod packer entry
+        hasCategory = False
+        categoryIndex = -1
+        tick = 0
+        for item in GLOBAL_JSON['mechs']:
+            if item['name'] == CATEGORY_NAME:
+                hasCategory = True
+                categoryIndex = tick-1
+                break
+
+            # Iterate
+            tick += 1
+        
+        # Add category if needed
+        if not hasCategory:
+            # Get index that will be added
+            categoryIndex = len(GLOBAL_JSON['mechs'])
+
+            # Add to category
+            GLOBAL_JSON['mechs'].append({'name' : CATEGORY_NAME, 'list' : []})
+
+        # Add Mod link to category
+        for category in GLOBAL_JSON['mechs']:
+            if category['name'] == CATEGORY_NAME:
+                category['list'].append(pathToStandard(path))
+
+        print(categoryIndex)
+        print(GLOBAL_JSON['mechs'])
+    # elif expression:
+    #     pass
+
+# Converts a local path from the mod packer folder to the Brigador standard
+def pathToStandard(path):
+    # Replace and send
+    return path.replace("../","assets/_modkit/")
 
 # Begin Operation
 if __name__ == '__main__':
